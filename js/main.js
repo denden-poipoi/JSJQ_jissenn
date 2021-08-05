1048b9bf5c2d6f7e0d0d4e63d042cab87d7f8e59
denden-poipoi <66948568+denden-poipoi@users.noreply.github.com>
2021-08-06 01:43:32 +0900

    完成

--- js/main.js
+++ js/main.js
@@ -37,3 +37,47 @@ $('.slideShow').slick({
   pauseOnHover: false, // マウスホバー時の一時停止の設定
   pauseOnDotsHover: false, // ドットマウスホバー時の一時停止の設定
 });
+
+
+// フェードイン
+function fadeIn(){
+  // フェードアップ
+  $('.fadeUpTrigger').each(function(){// fadeUpTriggerクラスの各要素に対して
+    let scroll = $(window).scrollTop();// スクロール位置を取得する
+    let triTop = $(this).offset().top + 100;// 要素の上部より100px下の位置を取得
+    let winHeight = $(window).height();// ウィンドウの高さを取得
+    if (scroll >= triTop - winHeight){ // 画面内に要素が入ったかを判断
+      $(this).addClass('fadeUp');// fadeUpクラスを付与
+    }else{
+      $(this).removeClass('fadeUp');// fadeUpクラスを削除
+    }
+  });
+
+  // フェードレフト
+  $('.fadeLeftTrigger').each(function(){
+    let scroll = $(window).scrollTop();
+    let triTop = $(this).offset().top + 100;
+    let winHeight = $(window).height();
+    if (scroll >= triTop - winHeight){
+      $(this).addClass('fadeLeft');
+    }else{
+      $(this).removeClass('fadeLeft');
+    }
+  });
+
+  // フェードライト
+  $('.fadeRightTrigger').each(function(){
+    let scroll = $(window).scrollTop();
+    let triTop = $(this).offset().top + 100;
+    let winHeight = $(window).height();
+    if (scroll >= triTop - winHeight){
+      $(this).addClass('fadeRight');
+    }else{
+      $(this).removeClass('fadeRight');
+    }
+  });
+}
+
+$(window).scroll(function () {// スクロールしたら
+  fadeIn();// 関数を実行
+});
